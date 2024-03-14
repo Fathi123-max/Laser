fc4fcedc7ae81fe1d1f6af4b09758fc5a85bb067
Fathi wehba <Fathiwehba5@gmail.com>
2024-03-14 19:13:33 +0400

    Add CheckboxWidget and CustomDialogFiled for order management
    Introduce CheckboxWidget and CustomDialogFiled components for better
    order management in the DialogScreen. This allows for holding orders
    and entering customer codes efficiently.

--- lib/app/modules/techome/views/widgets/acceptedorderspage.dart
+++ lib/app/modules/techome/views/widgets/acceptedorderspage.dart
@@ -8,6 +8,7 @@ import 'package:get/get.dart';
 import 'package:laser/app/components/custom_image_widget.dart';
 import 'package:laser/app/config/translations/localization_service.dart';
 import 'package:laser/app/data/models/techmodels/acceptedorders.dart';
+import 'package:laser/app/modules/Auth/controller/text_form_fied_controller.dart';
 import 'package:laser/app/modules/home/views/widgets/custom_divider.dart';
 import 'package:laser/app/modules/home/views/widgets/orders/custom_card_button.dart';
 import 'package:laser/app/modules/techome/controller/techomecontroller.dart';
@@ -686,26 +687,161 @@ class DialogScreen extends GetView<DialogScreen> {
             SizedBox(
               height: 16.h,
             ),
-            CheckboxListTile.adaptive(
-              value: true,
-              checkboxShape: const CircleBorder(),
-              controlAffinity: ListTileControlAffinity.leading,
-              title: Column(
-                children: [
-                  SizedBox(
-                    width: 250.w,
-                    child: Text(
-                      'Please enter customer’s\ncode to edit',
-                      style: textStyle,
-                    ),
-                  )
-                ],
-              ),
-              onChanged: (value) {},
+            CheckboxWidget(
+              textStyle: textStyle,
+              ishold: false,
+            ),
+            SizedBox(
+              height: 47.h,
+            ),
+            CheckboxWidget(
+              textStyle: textStyle,
+              ishold: true,
             )
           ],
         ),
       ),
     )));
+  }
+}
+
+class CheckboxWidget extends StatelessWidget {
+  final bool? ishold;
+
+  const CheckboxWidget({
+    super.key,
+    required this.textStyle,
+    this.ishold,
+  });
+
+  final TextStyle textStyle;
+
+  @override
+  Widget build(BuildContext context) {
+    return CheckboxListTile.adaptive(
+      value: true,
+      checkboxShape: const CircleBorder(),
+      controlAffinity: ListTileControlAffinity.leading,
+      title: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          SizedBox(
+            width: 250.w,
+            height: 40.h,
+            child: ishold ?? false
+                ? Text.rich(
+                    TextSpan(
+                      children: [
+                        TextSpan(
+                          text: 'Hold the order?\n',
+                          style: TextStyle(
+                            color: const Color(0xFF1B1926),
+                            fontSize: 14.sp,
+                            fontFamily: 'Inter',
+                            fontWeight: FontWeight.w700,
+                          ),
+                        ),
+                        TextSpan(
+                          text: 'Please mention the reason',
+                          style: TextStyle(
+                            color: const Color(0xFF1B1926),
+                            fontSize: 12.sp,
+                            fontFamily: 'Inter',
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+                : Text(
+                    'Please enter customer’s\ncode to edit',
+                    style: textStyle,
+                  ),
+          ),
+          CustomDialogFiled(
+            isRigter: ishold ?? false,
+            keyboardType: TextInputType.number,
+            hint: !ishold! ? "1234" : "Costumer didn’t respond",
+          )
+        ],
+      ),
+      onChanged: (value) {},
+    );
+  }
+}
+
+class CustomDialogFiled extends GetWidget<CustomTextFormFieldController> {
+  const CustomDialogFiled({
+    this.isRigter,
+    super.key,
+    this.textEditingController,
+    this.keyboardType,
+    this.hint,
+    this.isPassword = false,
+  });
+
+  final TextEditingController? textEditingController;
+  final String? hint;
+  final bool isPassword;
+  final bool? isRigter;
+
+  final TextInputType? keyboardType;
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        SizedBox(
+          width: 180.w,
+          height: isRigter! ? 91.h : 35.h,
+          child: Obx(() {
+            return TextField(
+              maxLines: 1000,
+              keyboardType: keyboardType,
+              controller: textEditingController,
+              obscureText: controller.isPasswordVisible.value && isPassword
+                  ? true
+                  : false,
+              decoration: InputDecoration(
+                hintText: hint,
+                fillColor: const Color(0xFFF1F0F5),
+                filled: true,
+                hintStyle: TextStyle(
+                  color: const Color(0xFF1B1926),
+                  fontSize: isRigter! ? 13.sp : 15.sp,
+                  fontFamily: 'Inter',
+                  fontWeight: FontWeight.w400,
+                  // height: 0.08,
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(30.r)),
+                  borderSide: const BorderSide(color: Colors.black),
+                ),
+                errorMaxLines: 1,
+                hintMaxLines: 3,
+                focusedErrorBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(30.r)),
+                  borderSide: const BorderSide(color: Colors.black),
+                ),
+                errorStyle: const TextStyle(
+                  height: 0,
+                  color: Color.fromRGBO(27, 25, 38, 1),
+                  fontFamily: 'Inter',
+                  letterSpacing: 0,
+                  fontWeight: FontWeight.normal,
+                ),
+                errorBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(30.r)),
+                  borderSide: const BorderSide(color: Colors.red, width: 2),
+                ),
+                contentPadding: EdgeInsets.fromLTRB(24.w, 10.h, 24.w, 12.h),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(30.r)),
+                ),
+              ),
+            );
+          }),
+        ),
+      ],
+    );
   }
 }
